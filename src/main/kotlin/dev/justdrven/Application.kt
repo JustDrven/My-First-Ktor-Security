5ce05c0bc750eda3258bad8764b99b41e3f2b433
JustDrven <petr.nemec11111@gmail.com>
2024-08-16 20:07:03 +0200

    Update checks

--- src/main/kotlin/dev/justdrven/Application.kt
+++ src/main/kotlin/dev/justdrven/Application.kt
@@ -1,5 +1,6 @@
 package dev.justdrven
 
+import dev.justdrven.managers.HashManager
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
@@ -7,8 +8,15 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun main(args: Array<String>) {
-    System.setProperty("token", "ahwefagwefhagwedfwahfeaw")
-    System.setProperty("api_key", "testAPIKey")
+    val tokenSource = "token"
+    val apiKeySource = "api_key"
+
+    System.setProperty(tokenSource, "ahwefagwefhagwedfwahfeaw")
+    System.setProperty(apiKeySource, "testAPIKey")
+
+    println("Encode token: " + HashManager.encode("ahwefagwefhagwedfwahfeaw"))
+    println("Encode api key: " + HashManager.encode("testAPIKey"))
+
     io.ktor.server.netty.EngineMain.main(args)
 }
 
@@ -35,14 +43,17 @@ fun Application.module() {
 
 suspend fun checkIfRequestAuthorization(call: ApplicationCall) {
     val auth = call.request.authorization().toString()
-    val value = (auth != "null" && auth == "Bearer ${System.getProperty("token")}")
+    val token = auth.replaceFirst("Bearer ", "")
+
+    val value = (auth != "null" && auth.startsWith("Bearer ") &&
+            HashManager.decode(token) == System.getProperty("token"))
     if (!value)
         call.respond(HttpStatusCode.Unauthorized, "Unauthorized!")
 }
 
 suspend fun checkIfRequestHasApiKey(call: ApplicationCall) {
-    val auth = call.request.headers["api_key"]
-    val value = (auth != "null" && auth.equals(System.getProperty("api_key")))
+    val auth = HashManager.decode(call.request.headers["api_key"].toString())
+    val value = (auth != "null" && auth == System.getProperty("api_key"))
     if (!value)
         call.respond(HttpStatusCode.NotFound, "Missing API Key")
 }
